d97c02fc2a460dca0371bdef43921a00137f3fdf
Jesse Jordan <jesselinnjordan@gmail>
2014-03-01 15:08:45 -0600

    First commit and READMEupdate

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made with my mentor at [Bloc](http://bloc.io)
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '34ef563a3edc70402c73073a96645041867ae079d698f25e88900eb5fdcaf096621eb0fcd49e52be3223bcedd4e02ac593f4649e4496cea1a482d2cef6b66e20'
