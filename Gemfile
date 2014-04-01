dff4a29fbcced0e59657202d499590e9c38a1ae4
Jesse Jordan <jesselinnjordan@gmail>
2014-04-01 15:40:20 -0500

    Installed and implemented Redcarpet for Markdown.

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'devise'
 
 gem 'cancan'
 
+gem 'redcarpet'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,11 @@ module ApplicationHelper
     else
       content_tag :div, capture(&block), class: 'control-group'
     end
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+    end  
   end
 end
