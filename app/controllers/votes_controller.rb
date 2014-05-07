d76a384abb43c93cab71eb9bcc4a907999df3dfe
Jesse Jordan <jesselinnjordan@gmail>
2014-05-06 21:04:26 -0500

    fixed vote controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,23 @@
 class VotesController < ApplicationController
   before_filter :setup
 
+  def up_vote
+    update_vote(1)
+    redirect_to :back
+  end
+
+  def down_vote
+    update_vote(-1)
+    redirect_to :back
+  end
+
   private
+
 
   def setup
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
-    authorize! :create, Vote, message: "You need to be a user to do that."
+    authorize! :create, Vote, message: "You need to be a member to vote."
 
     @vote = @post.votes.where(user_id: current_user.id).first
   end
@@ -17,28 +28,5 @@ class VotesController < ApplicationController
     else # create it
       @vote = current_user.votes.create(value: new_value, post: @post)
     end
-  end
-
-  def up_vote
-    update_vote(-1)
-    redirect_to :back
-    # Just like other controllers, grab the parent objects
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-
-    # Look for an existing vote by this person so we don't create multiple
-    @vote = @post.votes.where(user_id: current_user.id).first
-
-    if @vote # if it exists, update it
-      @vote.update_attribute(:value, 1)
-    else # create it
-      @vote = current_user.votes.create(value: 1, post: @post)
-    end
-    def down_vote
-      update_vote(-1)
-      redirect_to :back
-    end
-
-    redirect_to :back
   end
 end
