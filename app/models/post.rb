ce50c2b1e4ad503b11f802876ab3fd10c70ec48d
Jesse Jordan <jesselinnjordan@gmail>
2014-05-10 15:45:25 -0500

    Implemented favoriting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   belongs_to :user
   belongs_to :topic
   attr_accessible :body, :title, :topic, :image
@@ -9,6 +10,13 @@ class Post < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  default_scope order('rank DESC')
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true  
+  
   def up_votes
     self.votes.where(value: 1).count
   end
@@ -36,11 +44,5 @@ class Post < ActiveRecord::Base
     self.user.votes.create(value: 1, post: self)
   end 
 
-  default_scope order('rank DESC')
-
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true  
 end
 
