cb190840f5c66131c1940734fb77a3bc8ddb6eb2
Jesse Jordan <jesselinnjordan@gmail>
2014-05-11 20:38:22 -0500

    Interlude changes for registration, email permissions and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,10 @@ class User < ActiveRecord::Base
   def favorited(post)
     self.favorites.where(post_id: post.id).first
   end
+
+  def voted(post)
+    self.votes.where(post_id: post.id).first
+  end
     
 
   private
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,10 +5,16 @@ class Vote < ActiveRecord::Base
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
   after_save :update_post
 
-  private
+  def up_vote?
+    value == 1
+  end
 
+  def down_vote?
+    value == -1
+  end 
+
+  private
   def update_post
     self.post.update_rank 
-  end  
-
+  end
 end
