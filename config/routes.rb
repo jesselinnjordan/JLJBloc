d9a59d1bbcf123ac5365abe4bfeec7b2f706cee6
Jesse Jordan <jesselinnjordan@gmail>
2014-05-16 12:17:49 -0500

    Add all the stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
-
+  resources :users, only: [:show, :index]
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
